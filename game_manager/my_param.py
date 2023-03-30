d57dcbda1fbc005ac729ec67fa42ba2d0f01d825
iguzzi <iguchi@DESKTOP-SOF98RP.localdomain>
2023-03-31 01:26:01 +0900

    Level2 final

---
+++ game_manager/my_param.py
@@ -0,0 +1,9 @@
+# my_param.py for Level2 2023/3/30
+c_fullLines = 2.0
+c_nHoles = 10.0 #
+c_nIsolatedBlocks = 1.0
+c_absDy = 1.25
+c_maxDy = 0 #0.3
+c_maxHeight = 0 #5
+c_stdY = 0 #1.0
+c_stdDY = 0 #0.01
