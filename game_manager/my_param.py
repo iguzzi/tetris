da6904dbca72672fcefe1e6d9c63cd0f3b0e57bf
iguzzi <iguchi@DESKTOP-SOF98RP.localdomain>
2023-03-30 23:06:09 +0900

    parameters for Hold/Level3

--- game_manager/my_param.py
+++ game_manager/my_param.py
@@ -1,7 +1,8 @@
-c_fullLines = 5.0
-c_nHoles = 17.0
+# my_param.py for Level3 2023/3/30
+c_fullLines = 2.0
+c_nHoles = 6.0
 c_nIsolatedBlocks = 1.0
-c_absDy = 1.2
+c_absDy = 1.25
 c_maxDy = 0 #0.3
 c_maxHeight = 0 #5
 c_stdY = 0 #1.0
